dca6757d3b5ce40e6ce06ee334359c7e14323132
Parviz Ansaryan <ansaryanparviz@gmail.com>
2023-07-25 19:52:27 +0330

    fix: show modal title

--- resources/views/back/comments/index.blade.php
+++ resources/views/back/comments/index.blade.php
@@ -207,7 +207,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLongTitle">مشاهده جرئیات</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
