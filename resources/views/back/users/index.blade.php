9dc9de06e2d7c9509138fb5a28277bd76e64052b
ashkangh72 <ashkan.ghavami55@gmail.com>
2025-03-24 11:26:34 +0330

    a

--- resources/views/back/users/index.blade.php
+++ resources/views/back/users/index.blade.php
@@ -75,7 +75,7 @@
                         @csrf
                         @method('delete')
                         <button type="button" class="btn btn-success waves-effect waves-light" data-dismiss="modal">خیر</button>
-                        <button type="submit" class="btn btn-danger waves-effect waves-light" data-dismiss="modal">بله حذف شود</button>
+                        <button type="submit" class="btn btn-danger waves-effect waves-light">بله حذف شود</button>
                     </form>
                 </div>
             </div>
