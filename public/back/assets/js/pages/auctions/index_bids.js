c81841f42c198948d8aed3a59f54ebd07d5f5127
ashkangh72 <ashkan.ghavami55@gmail.com>
2025-05-02 23:33:19 +0330

    a

--- public/back/assets/js/pages/auctions/index_bids.js
+++ public/back/assets/js/pages/auctions/index_bids.js
@@ -55,100 +55,29 @@ let auction_datatable = function () {
             textAlign: 'center',
         },
         {
-            field: 'picture',
-            title: 'تصویر',
-            sortable: false,
-            width: 80,
-            template: function (row) {
-
-                return '<img class="post-thumb" src="' + row.picture + '" alt="' + row.title + '">';
-            }
-        },
-        {
-            field: 'title',
-            title: 'عنوان',
-            width: 150,
-            template: function (row) {
-                let title = ``;
-                if (row.type === 'محصول')
-                    title = `<a href="${row.url}">${row.title}</a> <span class="badge badge-info">${row.type}</span>`;
-                if (row.type === 'مزایده')
-                    title = `<a href="${row.url}">${row.title}</a> <span class="badge badge-primary">${row.type}</span>`;
-
-                return title;
-            }
-        },
-        {
-            field: 'sku',
-            title: 'شناسه مزایده',
-            width: 80,
-            template: function (row) {
-                return row.sku;
-            }
-        },
-        {
-            field: 'base_price',
-            title: 'قیمت پایه',
-            width: 150,
-            template: function (row) {
-                return row.base_price;
-            }
-        },
-        {
             field: 'username',
             title: 'نام کاربر',
             width: 150,
             template: function (row) {
-                return row.username;
+                return row.user.name;
             }
         },
         {
-            field: 'category',
-            title: 'دسته',
-            width: 100,
+            field: 'amount',
+            title: 'قیمت',
+            width: 150,
             template: function (row) {
-                return row.category;
-            }
-        },
-        {
-            field: 'status',
-            width: 80,
-            sortable: 'desc',
-            title: 'وضعیت',
-            template: function (row) {
-                let status = ``;
-                if (row.status === 'rejected')
-                    status = `<span class="badge badge-danger">رد شده</span><br/>`;//<small>دلیل: ${row.reject_reason}</small>`;
-                if (row.status === 'approved')
-                    status = `<span class="badge badge-success">تایید شده</span>`;
-                if (row.status === 'pending_approval')
-                    status = `<span class="badge badge-info">منتظر تایید</span>`;
-                return status;
+                return row.amount;
             }
         },
         {
             field: 'created_at',
-            width: 100,
+            width: 300,
             sortable: 'desc',
             title: 'تاریخ ثبت',
             template: function (row) {
-                return '<span class="ltr">' + row.updated_at + '</span>';
+                return '<span class="ltr">' + row.created_at + '</span>';
             }
-        },
-        {
-            field: 'actions',
-            title: 'عملیات',
-            textAlign: 'center',
-            sortable: false,
-            width: 150,
-            overflow: 'visible',
-            autoHide: false,
-            template: function (row) {
-                return `<a href="${row.links.show}" class="btn btn-info waves-effect waves-light">مشاهده</a>
-                <a href="${row.links.bids}" class="btn btn-info waves-effect waves-light">لیست پیشنهادات</a>
-                    <div class="btn-group-vertical"><a data-id="${row.id}" id="reject-btn" href ="${row.links.reject}"class="btn btn-outline-danger waves-effect waves-light">رد</a>
-                          <a data-id="${row.id}" id="accept-btn" href ="${row.links.accept}"class="btn btn-outline-success waves-effect waves-light">تایید</a></div>`;
-            },
         },
         ],
     };
@@ -203,116 +132,3 @@ jQuery(document).ready(function () {
     auction_datatable.init();
 });
 
-$('#auction-multiple-delete-form').on('submit', function (e) {
-    e.preventDefault();
-
-    $('#multiple-delete-modal').modal('hide');
-
-    let formData = new FormData(this);
-    let ids = datatable.checkbox().getSelectedId();
-
-    ids.forEach(function (id) {
-        formData.append('ids[]', id);
-    });
-
-    $.ajax({
-        url: $(this).attr('action'),
-        type: 'POST',
-        data: formData,
-        success: function (data) {
-            toastr.success('مزایده های انتخاب شده با موفقیت حذف شدند.');
-            datatable.reload();
-        },
-        beforeSend: function (xhr) {
-            block('#main-card');
-            xhr.setRequestHeader("X-CSRF-TOKEN", $('meta[name="csrf-token"]').attr('content'));
-        },
-        complete: function () {
-
-            unblock('#main-card');
-        },
-        cache: false,
-        contentType: false,
-        processData: false
-    });
-
-});
-$('#auction-accept-form').on('submit', function (e) {
-    e.preventDefault();
-
-    const id = $('#auction-accept-modal .modal-body').data('id');
-    $('#auction-accept-modal').modal('hide');
-    let formData = new FormData(this);
-    formData.append('id', id);
-
-    $.ajax({
-        url: $(this).attr('action'),
-        type: 'POST',
-        data: formData,
-        success: function (data) {
-            toastr.success('مزایده با موفقیت تایید شد.');
-            datatable.reload();
-        },
-        beforeSend: function (xhr) {
-            block('#main-card');
-            xhr.setRequestHeader("X-CSRF-TOKEN", $('meta[name="csrf-token"]').attr('content'));
-        },
-        complete: function () {
-
-            unblock('#main-card');
-        },
-        cache: false,
-        contentType: false,
-        processData: false
-    });
-
-});
-$(document).on("click", '#accept-btn', function (event) {
-    event.preventDefault();
-    const id = $(this).data('id');
-    $('#auction-accept-modal .modal-body').data('id', id);
-    $('#auction-accept-modal').modal('show');
-
-});
-
-$('#auction-reject-form').on('submit', function (e) {
-    e.preventDefault();
-
-    const id = $('#auction-reject-modal .modal-body').data('id');
-    const reason = $('#auction-reject-modal .modal-body input').val();
-
-    $('#auction-reject-modal').modal('hide');
-    let formData = new FormData(this);
-    formData.append('id', id);
-    formData.append('reason', reason);
-
-    $.ajax({
-        url: $(this).attr('action'),
-        type: 'POST',
-        data: formData,
-        success: function (data) {
-            toastr.success('مزایده با موفقیت رد شد.');
-            datatable.reload();
-        },
-        beforeSend: function (xhr) {
-            block('#main-card');
-            xhr.setRequestHeader("X-CSRF-TOKEN", $('meta[name="csrf-token"]').attr('content'));
-        },
-        complete: function () {
-
-            unblock('#main-card');
-        },
-        cache: false,
-        contentType: false,
-        processData: false
-    });
-
-});
-$(document).on("click", '#reject-btn', function (event) {
-    event.preventDefault();
-    const id = $(this).data('id');
-
-    $('#auction-reject-modal .modal-body').data('id', id);
-    $('#auction-reject-modal').modal('show');
-
-})
