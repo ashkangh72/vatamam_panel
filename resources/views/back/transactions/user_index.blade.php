18a4363095a3ddc77b544d8dd580ab573d79edd5
ashkangh72 <ashkan.ghavami55@gmail.com>
2025-08-18 22:21:06 +0330

    a

--- resources/views/back/transactions/user_index.blade.php
+++ resources/views/back/transactions/user_index.blade.php
@@ -82,8 +82,9 @@
                         <h4 class="card-title"></h4>
                         <h4 class="card-title"></h4>
                         <div>
+                            <h4 class="card-title">اطلاعات حساب: </h4>
                             @foreach ($user->bankAccounts as $item)
-                                <button class="btn btn-outline-primary">{{ $item->bank_name }}: {{ $item->iban }}</button> </br>
+                                <button class="btn btn-outline-primary max-w-300">{{ $item->bank_name }}: {{ $item->iban }}</button> </br></br>
                             @endforeach
                         </div>
                     </div>
